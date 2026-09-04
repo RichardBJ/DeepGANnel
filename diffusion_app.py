3e825ea8cf04544ab1cbe0e94ac975dcc0184c30
RichardBJ <rbj@liverpool.ac.uk>
2026-09-04 15:56:50 +0100

    recovered R analysis work from Experimental-LoR

--- diffusion_app.py
+++ diffusion_app.py
@@ -292,9 +292,6 @@ def run_training(
 
         coarse_blob = {
             "model_state_dict": coarse.state_dict(),
-            "history": chist,
-            "epochs": int(coarse_epochs),
-            "steps": int(cstep),
             "config": {"base_channels": int(base_channels), "time_dim": 128,
                        "timesteps": int(timesteps), "cond_channels": 4 + emb_dim},
             "norm": {"cond_mean": ccond_mean.ravel().tolist(),
